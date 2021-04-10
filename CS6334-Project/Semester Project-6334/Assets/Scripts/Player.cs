ec6e4c5e772cb6cf105887f68f0fe753b8093925
StufMuff <stuff_j@yahoo.com>
2021-04-10 17:10:46 -0500

    Current update of design project. Record room is function and save and load data is written but not tested. Still need to write date picker still needs to be wrote

--- CS6334-Project/Semester Project-6334/Assets/Scripts/Player.cs
+++ CS6334-Project/Semester Project-6334/Assets/Scripts/Player.cs
@@ -1,12 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-  public string name = "John Doe";
-  public string[] exercise = {"Squats","Twist-lung"};
-  public int[] sets = {3,2};
-  public int[] reps = {8,10};
+
+  // setting put player information that is saved
+  public string name = "John Doe";                //players name
+
+  // initializing exercise classes for the player
+  public PlayerData.Exercise exercise1 = new PlayerData.Exercise("sit-up");
+  public PlayerData.Exercise exercise2 = new PlayerData.Exercise("twist-crunches");
+  public PlayerData.Exercise exercise3 = new PlayerData.Exercise("twist-lung");
+
+  // setting default data for testing
+  void Start(){
+    // this is how you create new data into class
+    exercise1.generate_date("1/2/21", new int[] {8,20});
+    exercise1.generate_date("1/3/21", new int[] {12});
+    exercise1.generate_date("1/10/21", new int[] {14});
+    exercise2.generate_date("1/4/21", new int[] {7});
+    exercise2.generate_date("1/5/21", new int[] {9,11});
+    exercise2.generate_date("1/10/21", new int[] {13,15});
+    exercise3.generate_date("1/7/21", new int[] {3});
+    exercise3.generate_date("1/8/21", new int[] {1});
+    exercise3.generate_date("1/10/21", new int[] {5,6});
+    exercise3.generate_date("1/11/21", new int[] {7,8});
+    exercise3.generate_date("1/12/21", new int[] {9,10});
+    exercise3.generate_date("1/13/21", new int[] {11,12});
+  }
   
+  // function used save player information
+  public void SavePlayer()
+  {
+    SaveSystem.SavePlayer(this);
+  }
+  
+  //funciton used to load player information
+  public void LoadPlayer()
+  {
+    // pulling saved info
+    PlayerData data = SaveSystem.LoadPlayer();
+    
+    // updating player info
+    name = data.name;
+    exercise1 = data.exercise1;
+    exercise2 = data.exercise2;
+    exercise3 = data.exercise3;
+  }
 }
--- CS6334-Project/Semester Project-6334/Assets/Scripts/SaveSystem.cs
+++ CS6334-Project/Semester Project-6334/Assets/Scripts/SaveSystem.cs
@@ -2,34 +2,37 @@
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
+// creating static class to save/load information
 public static class SaveSystem
 {
+  // function used to save player info
   public static void SavePlayer(Player player)
   {
-    BinaryFormatter formatter = new BinaryFormatter();
-    string path = Application.persistentDataPath + "/player.sav";
-    FileStream stream = new FileStream(path, FileMode.Create);
+    BinaryFormatter formatter = new BinaryFormatter();            //creating binary formatter
+    string path = Application.persistentDataPath + "/player.sav"; //choosing a vaild path to save
+    FileStream stream = new FileStream(path, FileMode.Create);    //open file stream
 
-    PlayerData data = new PlayerData(player);
+    PlayerData data = new PlayerData(player);                     // putting current information into data
 
-    formatter.Serialize(stream, data);
-    stream.Close();
+    formatter.Serialize(stream, data);                            // saving file
+    stream.Close();                                               // close stream
   }
   
+  // function to load player
   public static PlayerData LoadPlayer()
   {
-    string path = Application.persistentDataPath + "/player.sav";
-    if (File.Exists(path))
+    
+    string path = Application.persistentDataPath + "/player.sav";   // go to save location
+    if (File.Exists(path))                                          // checking to see if file exist
     {
-      BinaryFormatter formatter = new BinaryFormatter();
-      FileStream stream = new FileStream(path, FileMode.Open);
+      BinaryFormatter formatter = new BinaryFormatter();            // creating formater
+      FileStream stream = new FileStream(path, FileMode.Open);      // open stream
       
-      PlayerData data = formatter.Deserialize(stream) as PlayerData;
+      PlayerData data = formatter.Deserialize(stream) as PlayerData;// loading information
       stream.Close();
       
       return data;
-    }else
-    {
+    }else{
       Debug.LogError("Save file not found in " + path);
       return null;
     }
