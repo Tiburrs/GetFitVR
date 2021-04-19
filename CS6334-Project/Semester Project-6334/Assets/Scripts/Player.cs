1d0c2a433203db6afc3337e3805d92b836178431
StufMuff <stuff_j@yahoo.com>
2021-04-19 18:54:11 -0500

    Record Viewing and Saving/Loading player information is completed.

--- CS6334-Project/Semester Project-6334/Assets/Scripts/Player.cs
+++ CS6334-Project/Semester Project-6334/Assets/Scripts/Player.cs
@@ -1,12 +1,105 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-  public string name = "John Doe";
-  public string[] exercise = {"Squats","Twist-lung"};
-  public int[] sets = {3,2};
-  public int[] reps = {8,10};
+
+  // setting put player information that is saved
+  // making static so it can persist
+  static protected string name = "Player";                //players name
+
+  // initializing exercise classes for the player
+  // making static so it can persist
+  static protected PlayerData.Exercise exercise1 = new PlayerData.Exercise("sit-up");
+  static protected PlayerData.Exercise exercise2 = new PlayerData.Exercise("twist-crunches");
+  static protected PlayerData.Exercise exercise3 = new PlayerData.Exercise("twist-lung");
+  static protected PlayerData.Exercise exercise4 = new PlayerData.Exercise("trash");
+
+  // setting default data for testing
+  void Start(){
+    // this is how you create new data into class
+//    exercise1.generate_date("1/2/2021", new int[] {8,20});
+//    exercise1.generate_date("1/3/2021", new int[] {12});
+//    exercise1.generate_date("1/10/2021", new int[] {14});
+//    exercise2.generate_date("1/4/2021", new int[] {7});
+//    exercise2.generate_date("1/5/2021", new int[] {9,11});
+//    exercise2.generate_date("1/10/2021", new int[] {13,15});
+//    exercise3.generate_date("1/7/2021", new int[] {3});
+//    exercise3.generate_date("1/8/2021", new int[] {1});
+//    exercise3.generate_date("1/10/2021", new int[] {5,6});
+//    exercise3.generate_date("1/11/2021", new int[] {7,8});
+//    exercise3.generate_date("1/12/2021", new int[] {9,10});
+//    exercise3.generate_date("1/13/2021", new int[] {11,12});
+  }
   
+  // method used to read name of player
+  static public string read_name()
+  {
+    return name;
+  }
+  // method used to read exercise class
+  // will need an int to reference an exercise to read as an argument
+  static public PlayerData.Exercise read_exercise(int exercise)
+  {
+    // switch case to find the exercise to return
+    switch(exercise)
+    {
+      case 0:
+        return exercise1;
+        break;
+      case 1:
+        return exercise2;
+        break;
+      case 2:
+        return exercise3;
+        break;
+      default:
+        Debug.LogError("invalid exercise to read");
+        return exercise4;
+        break;
+    }
+  }
+  // static method to add to othe exercise.
+  // will need to pass in an int to reference with exercise to change, the date as a string
+  // and the reps as an int array
+  static public void add_date(int exercise, string info, int[] reps_add)
+  {
+    // switch case to find the exercise to change
+    switch(exercise)
+    {
+      case 0:
+        exercise1.generate_date(info, reps_add);
+        break;
+      case 1:
+        exercise2.generate_date(info, reps_add);
+        break;
+      case 2:
+        exercise3.generate_date(info, reps_add);
+        break;
+      default:
+        Debug.LogError("invalid exercise to change");
+        break;
+    }
+  }
+  
+  // function used save player information
+  public void SavePlayer()
+  {
+    SaveSystem.SavePlayer();
+  }
+  
+  //funciton used to load player information
+  public void LoadPlayer()
+  {
+    // pulling saved info
+    PlayerData data = SaveSystem.LoadPlayer();
+    
+    // updating player info
+    name = data.name;
+    exercise1 = data.exercise1;
+    exercise2 = data.exercise2;
+    exercise3 = data.exercise3;
+  }
 }
--- CS6334-Project/Semester Project-6334/Assets/Scripts/PlayerData.cs
+++ CS6334-Project/Semester Project-6334/Assets/Scripts/PlayerData.cs
@@ -1,20 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
+// creating data that can be serialized and saved
 [System.Serializable]
 public class PlayerData
 {
-  public string name;
-  public string[] exercise;
-  public int[] sets;
-  public int[] reps;
   
-  public PlayerData (Player player)
+  [System.Serializable]
+  // a class used to track the sets and reps completed on a given day
+  public class Date{
+    
+    public string date;       // string containing data "month/day/year"
+    public int[] workout;     // int array containing all reps. each element is a different set
+    
+    // constructor function
+    // input a string for current date and int array for reps
+    public Date(string complete_date, int[] new_reps){
+      date = complete_date;
+      workout = new_reps;
+    }
+    
+  }// end of Date class
+  
+  [System.Serializable]
+  // a class used to keep track of each exercise
+  public class Exercise{
+    
+    public List<Date> dates = new List<Date>();   // creating list of Date classes to append information on the fly
+    public string exercise_name;                  // name of exercise
+    private string date_info = "";                // string used to build exisiting date information
+    
+    // constructor function
+    // takes string as input to set exercise_name
+    public Exercise(string name){
+      exercise_name = name;
+    }
+    
+    // if nothing is given, name is set to a blank string
+    public Exercise(){
+      exercise_name = "";
+    }
+    
+    // add a date class to your list
+    // takes current date and int array
+    public void generate_date(string complete_date, int[] reps){
+      dates.Add(new Date(complete_date, reps));
+    }
+    
+    // functiont that returns all date information as a string
+    // each set is seperated by a new line
+    // ex: "1/1/2021\tSet:\t1\tReps:\t1\n"
+    // date for each set is at the start of the line
+    public string get_dates_info(){
+      
+      // clear current date_info
+      date_info = "";
+      
+      // loop through each Date in list
+      foreach(Date date_cur in dates){
+        
+        // looping through int array
+        for(int i=0; i<date_cur.workout.Length;i++){
+          date_info += date_cur.date + "\tSet:\t" + (i+1) + "\tReps:\t" + date_cur.workout[i] + "\n";
+        }
+      }
+      
+      return date_info;
+    }
+    
+  }
+  
+  // creating variables the player will use
+  public string name;
+  
+  public Exercise exercise1 = new Exercise();
+  public Exercise exercise2 = new Exercise();
+  public Exercise exercise3 = new Exercise();
+  
+  // constructor to build player data
+  public PlayerData ()
   {
-    name = player.name;
-    exercise = player.exercise;
-    sets = player.sets;
-    reps = player.reps;
+    name = Player.read_name();
+    exercise1 = Player.read_exercise(0);
+    exercise2 = Player.read_exercise(1);
+    exercise3 = Player.read_exercise(2);
   }
 }
