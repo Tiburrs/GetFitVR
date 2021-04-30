3e78f98813bbfabb7918d632015ce8bf738f6da0
Umar Khalid <umark96@hotmail.com>
2021-04-30 09:05:24 -0500

    Adding changes for Compilation branch for Final Prototype

---
+++ CS6334-Project/Semester Project-6334/Assets/Scripts/DemonstrationOptions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonstrationOptions : MonoBehaviour
+{
+    // Reference the to canvas containing the demonstration
+    public Canvas demonstrationCanvas;
+
+    void Start()
+    {
+        demonstrationCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if(demonstrationCanvas.enabled == true)
+        {
+            // Listen for the user tapping the screen to exit the demonstrationCanvas
+            if(Input.GetButtonDown("Fire1"))
+                demonstrationCanvas.enabled = false;
+        }
+    }
+
+    // Function called by the onClick event trigger of the "Demonstration" button being gazed at.
+    public void CallDemonstration()
+    {
+        demonstrationCanvas.enabled = true;
+    }
+}
---
+++ CS6334-Project/Semester Project-6334/Assets/Scripts/WorkoutOptions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkoutOptions : MonoBehaviour
+{
+    // Reference the to canvas containing the workouts
+    public Canvas workoutOptionsCanvas;
+
+    void Start()
+    {
+        workoutOptionsCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if(workoutOptionsCanvas.enabled == true)
+        {
+            // Listen for the user tapping the screen to exit the workoutOptionsCanvas
+            if(Input.GetButtonDown("Fire1"))
+                workoutOptionsCanvas.enabled = false;
+        }
+    }
+
+    // Function called by the onClick event trigger of the "Workout" button being gazed at.
+    public void CallWorkoutOptions()
+    {
+        workoutOptionsCanvas.enabled = true;
+    }
+}
